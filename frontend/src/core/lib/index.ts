5383a75d857137c0b96f986213bc413fe37ab904
Ishita Lohani <ishita.lohani.4498@gmail.com>
2026-09-19 13:24:17 +0530

    Add missing frontend UI store

---
+++ frontend/src/core/lib/index.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { STORAGE_KEYS } from '@/config'
+
+interface UIState {
+  sidebarOpen: boolean
+  sidebarCollapsed: boolean
+  toggleSidebar: () => void
+  toggleSidebarCollapsed: () => void
+}
+
+export const useUIStore = create<UIState>()(
+  persist(
+    (set) => ({
+      sidebarOpen: false,
+      sidebarCollapsed: false,
+      toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
+      toggleSidebarCollapsed: () => set((state) => ({ sidebarCollapsed: !state.sidebarCollapsed })),
+    }),
+    { name: STORAGE_KEYS.UI },
+  ),
+)
